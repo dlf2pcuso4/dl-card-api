cf174e3f4949409e2c0fff579d0c4d266dd6fd18
dlf2pcuso4 <danielthkwong@gmail.com>
2022-09-13 21:26:08 +1000

    add server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const jsonServer = requrie("json-server");
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("carddb.json");
 const middlewares = jsonServer.defaults();
